1c93ce82283271a369160a81e29e1d0ed9ac599b
MyTKey78 <mkri@as3.no>
2025-03-09 23:23:34 +0100

    Update app.py
    ny epost adr

--- app.py
+++ app.py
@@ -70,4 +70,4 @@ with pd.ExcelWriter(excel_buffer, engine='xlsxwriter') as writer:
     df.to_excel(writer, sheet_name="Sykefraværskostnader", index=False)
 st.download_button(label="📥 Last ned som Excel", data=excel_buffer.getvalue(), file_name="sykefraværskostnader.xlsx", mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
 
-st.markdown("### 🔗 [Vil du ha hjelp til å få ned kostnadene? Ta kontakt med oss](https://www.as3.no)")
+st.markdown("### 🔗 [Vil du ha hjelp til å få ned kostnadene? Ta kontakt med oss ved å trykke her](https://https://www.as3.no/for-virksomheter/#sykefravr)")
